96362336f2ece6cc64eda84212563a3277080d55
Bruno Barba Venturi <barba3@gmail.com>
2023-09-30 09:42:48 -0700

    Authenticate with azure app secret (#1)
    * Update dns_azure2.sh: Authenticate with app secret
    * Update README.md

--- README.md
+++ README.md
@@ -3,14 +3,12 @@ An acme.sh Azure DNS API that doesn't require installing dependencies
 
 UNDER DEVELOPMENT
 
-What is working:
-* Authentication requires bearer token. You can get one here: https://learn.microsoft.com/en-us/rest/api/dns/record-sets/create-or-update
-* Create or update TXT record to verify domain ownership
-
 What is not working
-* Delete TXT record. This is not a priority because we create-or-update, so the old TXT records get replaced instead of appended to.
 * Configuration variables outside of the script. You need to modify the script source at the top to change the config variables for now.
 
 What do we need
-* Rename the config variable prefix to start with something nicer than MY_
-* A better authentication than bearer token.
+* Rename the config variable prefix to start with something nicer than MY_.
+* Move configuration away from the source code file.
+* Consider a mechanism to rotate credentials.
+* Consider a better authentication method than app secret.
+* User-friendly instructions for Azure DNS, app registration, and which values to copy-paste from Azure portal to script config variables.
--- dns_azure2.sh
+++ dns_azure2.sh
@@ -10,12 +10,15 @@
 
 # Please Read this guide first: https://github.com/acmesh-official/acme.sh/wiki/DNS-API-Dev-Guide
 
-MY_Token="[bearer token]"
-
+MY_AzureTenantId="[azure tenant id]"
 MY_AzureSubscriptionId="[azure subscription ID]"
+MY_AzureClientId="[app registration client id]"
+MY_AzureClientSecret="[app secret]"
 MY_AzureResourceGroupName="[name of the azure resource group containing your DNS zone]"
 MY_AzureDnsApiBaseUrl="https://management.azure.com"
+MY_AzureTokenUrl="https://login.microsoftonline.com/$MY_AzureTenantId/oauth2/v2.0/token"
 MY_AzureDnsZoneName="[your zone name, e.g. example.com]"
+MY_AzureScope="https://management.azure.com/.default"
 
 #Usage: dns_myapi_add   _acme-challenge.www.domain.com   "XKrxpRBosdIKFzxW_CT3KLZNf6q0HG9i01zxXp5CPBs"
 dns_myapi_add() {
@@ -28,20 +31,26 @@ dns_myapi_add() {
         fulldomain=$1
         txtvalue=$2
 
-        _debug "First parse the zone names"
+        _debug "Parse the zone names"
         _domain=$MY_AzureDnsZoneName
         _sub_domain="${fulldomain/.$_domain/}"
 
         _debug _sub_domain $_sub_domain
         _debug _domain "$_domain"
 
+        _info "Authenticating with Azure"
+        _auth_result="$(_post "client_id=$MY_AzureClientId&scope=$MY_AzureScope&client_secret=$MY_AzureClientSecret&grant_type=client_credentials" "$MY_AzureTokenUrl" "" "POST")"
+
+        _secure_debug _auth_result "$_auth_result"
+        _access_token="${_auth_result#*\"access_token\":\"}"
+        _access_token="${_access_token%%\"*}"
+        _secure_debug _access_token $_access_token
+
         _info "Adding record"
-        if _invoke_rest PUT "subscriptions/$MY_AzureSubscriptionId/resourceGroups/$MY_AzureResourceGroupName/providers/Microsoft.Network/dnsZones/$MY_AzureDnsZoneName/TXT/$_sub_domain?api-version=2018-05-01" "{ \"properties\": { \"TTL\": 3600, \"TXTRecords\": [ { \"value\": [ \"$txtvalue\" ] } ] } }"; then
+        if _invoke_rest PUT "$MY_AzureDnsApiBaseUrl/subscriptions/$MY_AzureSubscriptionId/resourceGroups/$MY_AzureResourceGroupName/providers/Microsoft.Network/dnsZones/$MY_AzureDnsZoneName/TXT/$_sub_domain?api-version=2018-05-01" "{ \"properties\": { \"TTL\": 3600, \"TXTRecords\": [ { \"value\": [ \"$txtvalue\" ] } ] } }" "$_access_token"; then
+                _debug response $response
                 if _contains "$response" "$txtvalue"; then
                         _info "Added, OK"
-                        return 0
-                elif _contains "$response" "The record already exists"; then
-                        _info "Already exists, OK"
                         return 0
                 else
                         _err "Add txt record error."
@@ -73,16 +82,17 @@ _invoke_rest() {
         m=$1
         ep="$2"
         data="$3"
+        accessToken="$4"
         _debug "$ep"
 
         export _H1="Content-Type: application/json"
-        export _H2="Authorization: Bearer $MY_Token"
+        export _H2="Authorization: Bearer $accessToken"
 
         if [ "$m" != "GET" ]; then
                 _debug data "$data"
-                response="$(_post "$data" "$MY_AzureDnsApiBaseUrl/$ep" "" "$m")"
+                response="$(_post "$data" "$ep" "" "$m")"
         else
-                response="$(_get "$MY_AzureDnsApiBaseUrl/$ep")"
+                response="$(_get "$ep")"
         fi
 
         if [ "$?" != "0" ]; then
